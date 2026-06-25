b450502acd5bec836405a02cc54a73c9eeb4a698
Muhammad Zain <muhammadzainf15@gmail.com>
2026-06-25 10:02:43 +0500

    "Fix Running Error and add relative path"

--- scripts/bronze/load_data_bronze.py
+++ scripts/bronze/load_data_bronze.py
@@ -1,7 +1,7 @@
 # ====================================
 # Purpose: This Python script load data from the external csv files into mysql tables
 # It Truncate the tables and load Data from csv files
-# Warning: Path of files must be kept same or change for proper data ingestion
+# Warning: Path of files must be kept same for proper data ingestion
 # ====================================
 
 import mysql.connector
@@ -39,41 +39,49 @@ def load_data(table_name,file_path):
     return time_difference
     
 
-cnx = mysql.connector.connect(
-    user = "root", password = "password",
-    host = "localhost", database = "bronze",
-    allow_local_infile = True
-)
+try:
+    cnx = mysql.connector.connect(
+        user = "root", password = "password",
+        host = "localhost", database = "bronze",
+        allow_local_infile = True,
+        use_pure = True
+    )
 
-cursor = cnx.cursor()
+    cursor = cnx.cursor()
 
-# Fetch table names of Bronze Layer
-tables = cursor.execute('SHOW TABLES')
-tables = cursor.fetchall()
+    # Fetch table names of Bronze Layer
+    tables = cursor.execute('SHOW TABLES')
+    tables = cursor.fetchall()
 
-# Path of Files to load data from
-files_path = [
-    "D:/source_crm/cust_info.csv",
-    "D:/source_crm/prd_info.csv",
-    "D:/source_crm/sales_details.csv",
-    "D:/source_erp/cust_az12.csv",
-    "D:/source_erp/loc_a101.csv",
-    "D:/source_erp/px_cat_g1v2.csv"
-    ]
+    # Path of Files to load data from
+    files_path = [
+        "./../../datasets/source_crm/cust_info.csv",
+        "./../../datasets/source_crm/prd_info.csv",
+        "./../../datasets/source_crm/sales_details.csv",
+        "./../../datasets/source_erp/cust_az12.csv",
+        "./../../datasets/source_erp/loc_a101.csv",
+        "./../../datasets/source_erp/px_cat_g1v2.csv"
+        ]
 
-print("="*20)
-print("LOADING DATA INTO BRONZE LAYER")
-print("="*20)
+    print("="*20)
+    print("LOADING DATA INTO BRONZE LAYER")
+    print("="*20)
 
-total_loading_time = 0
+    total_loading_time = 0
 
-for i in range(len(files_path)):
-    total_loading_time+= load_data(tables[i][0],files_path[i])
+    for i in range(len(files_path)):
+        total_loading_time+= load_data(tables[i][0],files_path[i])
 
-print('='*50)
-print(f"Total Loading time of Bronze layer = {total_loading_time:.2f}")
-print('='*50)
+    print('='*50)
+    print(f"Total Loading time of Bronze layer = {total_loading_time:.2f}")
+    print('='*50)
 
-cnx.commit()
-cursor.close()
-cnx.close()
+    cnx.commit()
+    cursor.close()
+    cnx.close()
+
+except mysql.connector.Error as e:
+    print(f"Error: {e}")
+
+
+
