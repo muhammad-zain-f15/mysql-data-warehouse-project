8a8dfbb4c0ce01a7cc41c1c1b35ea26eb2317e92
Muhammad Zain <muhammadzainf15@gmail.com>
2026-08-06 08:29:17 +0500

    update silver schema tables quality checks

--- tests/quality_checks_silver.sql
+++ tests/quality_checks_silver.sql
@@ -21,19 +21,36 @@ HAVING count(*) > 1;
 SELECT cst_firstname,
     cst_lastname
 FROM silver.crm_cust_info
-where cst_firstname <> TRIM(cst_firstname)
-    or cst_lastname <> TRIM(cst_lastname);
+where cst_firstname != TRIM(cst_firstname)
+    or cst_lastname != TRIM(cst_lastname);
 -- Data Standardization and Consistency
 SELECT DISTINCT cst_gndr
 FROM silver.crm_cust_info;
 -- ==============================================
 -- Checking silver.crm_prd_info
 -- ==============================================
+select
+prd_id,
+count(*)
+from silver.crm_prd_info
+group by prd_id
+having count(*) > 1
+
+select
+prd_nm
+from silver.crm_prd_info
+where prd_nm != trim(prd_nm)
+    
 -- check for negative numbers
 -- result : no output
 SELECT prd_cost
 FROM silver.crm_prd_info
 WHERE prd_cost < 0;
+
+select
+prd_cost
+from silver.crm_prd_info
+where prd_cost is null
 -- Data Standardization and Consistency
 SELECT distinct prd_line
 FROM silver.crm_prd_info;
@@ -45,10 +62,50 @@ where prd_start_dt > prd_end_dt;
 -- ==============================================
 -- Checking silver.crm_sales_details
 -- ==============================================
+select
+sls_prd_key
+from silver.crm_sales_details
+where sls_prd_key not in (select prd_key from silver.crm_prd_info)
+
+select
+sls_cust_id
+from silver.crm_sales_details
+where sls_cust_id not in (select cst_id from silver.crm_cust_info)
 -- Check Date Consistency
-select sls_order_dt
-from
-silver.crm_sales_details;
+select
+sls_order_dt
+from silver.crm_sales_details
+where len(sls_order_dt) < 8 
+
+select
+*
+from silver.crm_sales_details
+where sls_ship_dt > sls_due_dt
+
+select
+sls_due_dt
+from silver.crm_sales_details
+where len(sls_due_dt) < 8 
+
+select
+*
+from silver.crm_sales_details
+where sls_sales <0
+
+select
+*
+from silver.crm_sales_details
+where sls_sales != sls_quantity * sls_price
+
+select
+*
+from silver.crm_sales_details
+where sls_price < 0
+
+select
+*
+from silver.crm_sales_details
+where sls_price is null
 
 -- ==============================================
 -- Checking silver.erp_cust_az12
@@ -64,6 +121,7 @@ select
 DISTINCT gen
 from
 silver.erp_cust_az12;
+
 -- ==============================================
 -- Checking silver.erp_loc_a101
 -- ==============================================
@@ -71,6 +129,10 @@ silver.erp_cust_az12;
 select
 DISTINCT cntry
 from silver.erp_loc_a101;
+
+select
+distinct cntry
+from silver.erp_loc_a101
 -- ==============================================
 -- Checking silver.erp_px_cat_g1v2
 -- ==============================================
