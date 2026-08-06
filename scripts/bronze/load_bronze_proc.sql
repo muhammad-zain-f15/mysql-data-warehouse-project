337e4603f91be24f794ae8b4dcb3f4ee46012867
Muhammad Zain <muhammadzainf15@gmail.com>
2026-08-06 08:23:41 +0500

    update load bronze data SQL script

---
+++ scripts/bronze/load_bronze_proc.sql
@@ -0,0 +1,65 @@
+-- usage: EXEC bronze.load_bronze
+
+CREATE OR ALTER PROCEDURE bronze.load_bronze AS
+BEGIN
+	TRUNCATE TABLE bronze.crm_cust_info
+	BULK INSERT bronze.crm_cust_info
+	FROM 'E:\portfolio_projects\mysql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+	WITH(
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+
+	);
+
+	TRUNCATE TABLE bronze.crm_prd_info
+	BULK INSERT bronze.crm_prd_info
+	FROM 'E:\portfolio_projects\mysql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+	WITH(
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+
+	);
+
+	TRUNCATE TABLE bronze.crm_sales_details
+	BULK INSERT bronze.crm_sales_details
+	FROM 'E:\portfolio_projects\mysql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+	WITH(
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+
+	);
+
+	TRUNCATE TABLE bronze.erp_cust_az12
+	BULK INSERT bronze.erp_cust_az12
+	FROM 'E:\portfolio_projects\mysql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
+	WITH(
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+
+	);
+
+	TRUNCATE TABLE bronze.erp_loc_a101
+	BULK INSERT bronze.erp_loc_a101
+	FROM 'E:\portfolio_projects\mysql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
+	WITH(
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+
+	);
+
+	TRUNCATE TABLE bronze.erp_px_cat_g1v2
+	BULK INSERT bronze.erp_px_cat_g1v2
+	FROM 'E:\portfolio_projects\mysql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
+	WITH(
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+
+	);
+END
+
